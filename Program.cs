db4f8a05891bc80e0846ac0aebbcdfc9ee78b32b
Bernardo <bemeine15@hotmail.com>
2023-09-19 16:14:03 -0300

    Connecting to SQL Server and running first migration

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace SalesWebMVC
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
-
+            
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
